7d3976d23e2e37789042808ef4e76173e1ac0710
Marc François <fmarc@MacBook-Pro-de-Marc.local>
2021-04-13 21:44:22 +0200

    ajout des infos perso dans le layering

--- Front/src/components/ProfilPage/ProfilPage.js
+++ Front/src/components/ProfilPage/ProfilPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import LeftMenu from '../LeftMenu/LeftMenu';
 import './styles.scss';
 import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
 
 const ProfilPage = () => {
 
@@ -32,14 +33,46 @@ const ProfilPage = () => {
                     justifyContent="center"
                     >
                         <Box
-                        width="90%"
+                        p={8}
                         height="80%"
                         bgcolor="white"
                         boxShadow={3}
                         borderRadius="10px"
+                        className="profil-box"
 
                         >
-                            <h1>Salut</h1>
+                            <h1 className="profil-box__main-title">Vos informations</h1>
+                            <div className="profil-box__content">
+                                <div className="profil-box__content-elt">
+                                    <p>Nom de l'organisme:</p>
+                                    <p>Hôpital Paris Saint-Joseph</p>
+                                </div>
+                                <Divider/>
+                                <div className="profil-box__content-elt">
+                                    <p>N° de téléphonne:</p>
+                                    <p>01.44.12.33.33</p>
+                                </div>
+                                <Divider/>
+                                <div className="profil-box__content-elt">
+                                    <p>RPSS:</p>
+                                    <p>680004546</p>
+                                </div>
+                                <Divider/>
+                                <div className="profil-box__content-elt">
+                                    <p>Ville:</p>
+                                    <p>Paris</p>
+                                </div>
+                                <Divider/>
+                                <div className="profil-box__content-elt">
+                                    <p>Adresse:</p>
+                                    <p>185 Rue Raymond Losserand</p>
+                                </div>
+                                <Divider/>
+                                <div className="profil-box__content-elt">
+                                    <p>Code postal:</p>
+                                    <p>75 014</p>
+                                </div>
+                            </div>
                         </Box>
                     </Box>
             </Box>
