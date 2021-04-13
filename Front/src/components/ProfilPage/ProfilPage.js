0cc7586d9b683c94d8a2552870ba9c9559fffc8a
Marc François <fmarc@MacBook-Pro-de-Marc.local>
2021-04-13 22:10:34 +0200

    fin du layering de la page profil

--- Front/src/components/ProfilPage/ProfilPage.js
+++ Front/src/components/ProfilPage/ProfilPage.js
@@ -3,6 +3,8 @@ import LeftMenu from '../LeftMenu/LeftMenu';
 import './styles.scss';
 import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
 
 const ProfilPage = () => {
 
@@ -34,7 +36,6 @@ const ProfilPage = () => {
                     >
                         <Box
                         p={8}
-                        height="80%"
                         bgcolor="white"
                         boxShadow={3}
                         borderRadius="10px"
@@ -44,32 +45,43 @@ const ProfilPage = () => {
                             <h1 className="profil-box__main-title">Vos informations</h1>
                             <div className="profil-box__content">
                                 <div className="profil-box__content-elt">
-                                    <p>Nom de l'organisme:</p>
+                                    <p className="profil-box__content-elt__infos">Nom de l'organisme:</p>
                                     <p>Hôpital Paris Saint-Joseph</p>
                                 </div>
                                 <Divider/>
                                 <div className="profil-box__content-elt">
-                                    <p>N° de téléphonne:</p>
+                                    <p className="profil-box__content-elt__infos">e-mail:</p>
+                                    <IconButton aria-label="delete">
+                                        <EditIcon />
+                                    </IconButton>
+                                    <p>hopitalsj@sante-paris.fr</p>
+                                </div>
+                                <Divider/>
+                                <div className="profil-box__content-elt">
+                                    <p className="profil-box__content-elt__infos">N° de téléphonne:</p>
+                                    <IconButton aria-label="delete">
+                                        <EditIcon />
+                                    </IconButton>
                                     <p>01.44.12.33.33</p>
                                 </div>
                                 <Divider/>
                                 <div className="profil-box__content-elt">
-                                    <p>RPSS:</p>
+                                    <p className="profil-box__content-elt__infos">RPSS:</p>
                                     <p>680004546</p>
                                 </div>
                                 <Divider/>
                                 <div className="profil-box__content-elt">
-                                    <p>Ville:</p>
+                                    <p className="profil-box__content-elt__infos">Ville:</p>
                                     <p>Paris</p>
                                 </div>
                                 <Divider/>
                                 <div className="profil-box__content-elt">
-                                    <p>Adresse:</p>
+                                    <p className="profil-box__content-elt__infos">Adresse:</p>
                                     <p>185 Rue Raymond Losserand</p>
                                 </div>
                                 <Divider/>
                                 <div className="profil-box__content-elt">
-                                    <p>Code postal:</p>
+                                    <p className="profil-box__content-elt__infos">Code postal:</p>
                                     <p>75 014</p>
                                 </div>
                             </div>
