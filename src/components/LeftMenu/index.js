40520f0fe569e9770a8d0adced75fa0373688c59
geoffrey <geoffrey.maillot@gmail.com>
2021-04-29 14:39:12 +0200

    icone left menu

--- src/components/LeftMenu/index.js
+++ src/components/LeftMenu/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Import react-router-dom pour ajouter des links aux boutons
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 // Import des composants depuis MATERIAL UI
 
@@ -19,7 +19,10 @@ import Badge from '@material-ui/core/Badge';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import TableChartIcon from '@material-ui/icons/TableChart';
-import avatarImg from '../../assets/img_avatar.png';
+
+// Image
+import pharmacy from 'src/assets/img/pharmacie.svg';
+import hopital from 'src/assets/img/hopital.svg';
 
 // Import CSS
 import './styles.scss';
@@ -67,7 +70,12 @@ const LeftMenu = ({ userType, nbOfArticles, handleLogout, establishment }) => {
       bgcolor="#bfcee2"
       className="left-menu"
     >
-      <Avatar alt="avatarLogo" src={avatarImg} className="left-menu__avatar" />
+      <Avatar
+        alt="avatarLogo"
+        src={userType === 'pharmacy' ? pharmacy : hopital}
+        className="left-menu__avatar"
+        variant="rounded"
+      />
 
       <Typography variant="h5" component="h5" className="left-menu__welcome-message" align="center">
         Bienvenue <br />
